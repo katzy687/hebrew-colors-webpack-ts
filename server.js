ad01ed36a4cb75c71e8c0724b9a75f895f829e01
Natti Katz <katzy687@gmail.com>
2017-05-11 12:52:11 +0300

    'made carousel page responsive to resize'

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var config = require('./config');
-var setupController = require('./controllers/setupController');
-var apiController = require('./controllers/apiController');
-
-// for launching in browser
-var open = require('open');
 
 var port = process.env.PORT || 4000;
 
@@ -14,12 +8,6 @@ app.use('/', express.static(__dirname + '/src'));
 app.use(express.static('node_modules'));
 
 
-// mongoose.connect(config.getDbConnectionString());
-// setupController(app);
-// apiController(app);
-
 app.listen(port, function(){
   console.log('up and running on port ' + port);
-  console.log('Launching Browser!');
-  open("http:localhost:" + port);
 });
