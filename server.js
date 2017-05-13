2bdc06531aef124630b29a3d42e6b0a508a8e31a
Natti Katz <katzy687@gmail.com>
2017-05-13 13:28:26 +0300

    'heroku test'

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var path = require('path');
 var app = express();
 var open = require('open');
 
+// for now just serve up everything
 app.use(express.static(path.resolve(__dirname)));
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function(){
   console.log('server started on port ' + port);
   console.log('Launching Browser!');
