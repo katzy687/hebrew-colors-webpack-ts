9deb51d9354a65a9ef402d11dcf640dce547f144
Natti Katz <katzy687@gmail.com>
2017-05-13 13:24:00 +0300

    'responsive media queries for tooltips'

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var open = require('open');
 
 app.use(express.static(path.resolve(__dirname)));
 
 var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started on port ' + port);
+app.listen(port, function(){
+  console.log('server started on port ' + port);
+  console.log('Launching Browser!');
+  open("http:localhost:" + port);
+});
